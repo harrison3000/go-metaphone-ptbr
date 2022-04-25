a6b8bb3bfd04d98a02ddcf8d292ed4153d5a1746
harrison3000 <harrison3000@users.noreply.github.com>
2022-04-25 17:15:17 -0300

    adiciona letras l, t, p e altera o loop pra ficar mais parecido com o original

--- metaphone.go
+++ metaphone.go
@@ -32,7 +32,7 @@ import (
 	"unicode"
 )
 
-func Metaphone_PTBR_s(s string, separator rune) string {
+func Metaphone_PTBR_s(s string, max_length int, separator rune) string {
 	s = makeUpperAndClean(s)
 
 	primary := &strings.Builder{}
@@ -41,15 +41,17 @@ func Metaphone_PTBR_s(s string, separator rune) string {
 	WORD_EDGE := func(c rune) bool {
 		return c == 0 || unicode.IsSpace(c) || c == separator
 	}
+	GetAt := getAt
 
 	var last_char rune
 
-	r := []rune(s)
-	r = append(r, 0) //NULL terminator falso, pra WORD_EDGE funcionar, entre outras coisas
+	original := []rune(s)
+	original = append(original, 0) //NULL terminator falso, pra WORD_EDGE funcionar, entre outras coisas
 
 	//Neste loop eu tentei ao maximo preservar os nome de variaveis e
 	//comentários da versão original em C
-	for current, current_char := range r {
+	for current := 0; primary.Len() < max_length && current < len(original); {
+		current_char := GetAt(original, current)
 
 		if separator == current_char {
 			MetaphAddChr(primary, separator)
@@ -63,11 +65,38 @@ func Metaphone_PTBR_s(s string, separator rune) string {
 			if WORD_EDGE(last_char) {
 				MetaphAddChr(primary, current_char)
 			}
+		case 'L':
+			ahead_char := GetAt(original, current+1)
+			/* lha, lho. Adicionado 2009-11-09. Thx Peter Krauss. Ele estava mal-colocado */
+			if ahead_char == 'H' {
+				MetaphAddChr(primary, '1')
+			} else if isVowel(ahead_char) || WORD_EDGE(last_char) {
+				/* como em Louco, aloprado, alado, lampada, etc */
+				MetaphAddChr(primary, 'L')
+			}
+			/* atualmente ignora L antes de consoantes */
 
+		case 'T', 'P':
+			/* those are special cases, from foreign names or
+			 * old portuguese names sintax.
+			 * Besides, should behavior as the others.
+			 */
+			ahead_char := GetAt(original, current+1)
+			if ahead_char == 'H' {
+				/* phone, pharmacia, teophilo */
+				if current_char == 'P' {
+					MetaphAddChr(primary, 'F')
+				} else {
+					MetaphAddChr(primary, 'T')
+					current++
+				}
+			}
 			//TODO continuar tradução
 			_ = current
 		}
 
+		/* next char */
+		current++
 		last_char = current_char
 	}
 
